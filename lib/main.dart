effa9f441d9dc868ab23a61e85de99da7beefef7
tk <tykeaboyloy@gmail.com>
2024-08-26 10:11:45 +0700

    close #3 added google oauth

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ import 'package:monnaie/view/home/my_home_page.dart';
 import 'widgets/styled_button.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
